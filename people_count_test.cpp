61702d3ff48c16a72dc215944b7d83dc06f47dcc
stereomatchingkiss <thamngapwei@gmail.com>
2017-02-04 12:31:18 +0800

    1 : use blob_detector_base to replace blob detection chores, more modularize 2 : remove useless parameters 3 : refine variables name, easier to read

--- people_count_test.cpp
+++ people_count_test.cpp
@@ -1,4 +1,5 @@
 #include "people_count_test.hpp"
+#include "bg_subtract_detector.hpp"
 #include "dlib_tracker.hpp"
 #include "opencv_trackers.hpp"
 
@@ -44,10 +45,11 @@ people_count_test::~people_count_test()
 
 void people_count_test::init_data()
 {
-    process_frame_ = 0;
     in_blob_size_ = 0;
     out_blob_size_ = 0;
-    fgbg_ = cv::createBackgroundSubtractorMOG2(500, 60, true);
+    double constexpr anchor_ratio = 0.5;
+    blob_detector_ = std::make_unique<bg_subtract_detector>(cv::createBackgroundSubtractorMOG2(500, 60, true),
+                                                            min_human_size_, anchor_ratio, detect_margin_);
 
     auto legal_roi_strategy = [this](auto const &r)
     {
@@ -63,93 +65,39 @@ void people_count_test::init_data()
         }
     };
 
-    //tracker_ = std::make_unique<opencv_trackers>(legal_roi_strategy,
-    //                                             lost_track_strategy, "KCF");
-    tracker_ = std::make_unique<dlib_tracker>(legal_roi_strategy, lost_track_strategy);
+    tracker_ = std::make_unique<opencv_trackers>(legal_roi_strategy,
+                                                 lost_track_strategy, "KCF");
+    //tracker_ = std::make_unique<dlib_tracker>(legal_roi_strategy, lost_track_strategy);
 }
 
 void people_count_test::draw_measure_line()
 {
     int const ypos = img_.rows / 2;
-    cv::line(img_, {0, ypos - offset_}, {img_.cols, ypos - offset_}, {255, 0, 0});
+    cv::line(img_, {0, ypos - detect_margin_}, {img_.cols, ypos - detect_margin_}, {255, 0, 0});
     cv::line(img_, {0, ypos}, {img_.cols, ypos}, {255, 0, 0});
-    cv::line(img_, {0, ypos + offset_}, {img_.cols, ypos + offset_}, {255, 0, 0});
+    cv::line(img_, {0, ypos + detect_margin_}, {img_.cols, ypos + detect_margin_}, {255, 0, 0});
 }
 
-void people_count_test::preprocess_input()
-{
-    cv::cvtColor(img_, gray_img_, CV_BGR2GRAY);
-    fgbg_->apply(gray_img_, fg_);
-}
-
-void people_count_test::preprocess_fg(cv::Mat const &kernel)
-{
-    cv::GaussianBlur(fg_, fg_, {5,5}, 0);
-    cv::threshold(fg_, fg_, 127, 255, cv::THRESH_BINARY);
-    cv::dilate(fg_, fg_, kernel);
-    if(debug_){
-        fg_.copyTo(contour_img_);
-    }
-    cv::findContours(fg_, contours_, cv::RETR_EXTERNAL,
-                     cv::CHAIN_APPROX_SIMPLE);
-    if(debug_){
-        contour_img_.copyTo(fg_);
-        contour_img_.setTo(0);
-    }
-}
-
-void people_count_test::remove_outlier_blobs()
-{
-    int const key_point = img_.rows / 2;
-    auto it = std::remove_if(std::begin(bound_rects_), std::end(bound_rects_),
-                             [=](cv::Rect const &r)
-    {
-        auto const center = ocv::rect_center(r);
-        return center.y < key_point - offset_ ||
-                center.y > key_point + offset_ ||
-                r.area() < min_human_size_;
-    });
-    bound_rects_.erase(it, std::end(bound_rects_));
-}
-
-void people_count_test::track_people(cv::Mat const &kernel)
-{
-    preprocess_fg(kernel);
-    //cv::drawContours(contour_img_, contours_, -1, {255}, -1);
-    ocv::contours_to_bounding_rect(contours_, bound_rects_);
-    remove_outlier_blobs();
-    tracker_->match_existing_rects(img_, bound_rects_);
-    tracker_->draw_track_blobs(img_, img_);
-    put_inout_msg();
-    if(debug_){
-        cv::imshow("contour_img", contour_img_);
-        cv::imshow("fg", fg_);
-    }
-}
-
-void people_count_test::track(std::string const &video,
-                              size_t neglect_frame_size)
+void people_count_test::track(std::string const &video)
 {
     cv::VideoCapture cap(video);
     if(cap.isOpened()){
-        init_data();
-        cv::Mat const kernel = cv::getStructuringElement(cv::MORPH_RECT, {7,7});
+        init_data();        
         cv::VideoWriter vwrite = create_video_writer(video);
         cap>>img_;
         for(; !img_.empty(); cap>>img_){
-            preprocess_input();
             draw_measure_line();
-            if(process_frame_ > neglect_frame_size){
-                track_people(kernel);
-            }
+            blob_detector_->detect_blob(img_);
+            tracker_->match_existing_rects(img_, blob_detector_->get_roi());
+            tracker_->draw_track_blobs(img_, img_);
+            put_inout_msg();
             cv::imshow("img", img_);
             vwrite.write(img_);
 
             int const c = cv::waitKey(30);
             if (c == 'q' || c == 'Q' || (c & 255) == 27){
                 break;
-            }
-            std::cout<<"frame "<<process_frame_++<<std::endl;
+            }            
         }
     }else{
         std::cerr<<"cannot open "<<video<<"\n";
@@ -166,5 +114,5 @@ void people_count_test::put_inout_msg()
 
 void people_count_test::test_00(std::string const &file)
 {
-    track(file, 10);
+    track(file);
 }
--- people_count_test.hpp
+++ people_count_test.hpp
@@ -7,6 +7,7 @@
 
 #include <memory>
 
+class blob_detector_base;
 class tracker_base;
 
 class people_count_test
@@ -21,27 +22,17 @@ public:
 private:
     void draw_measure_line();
     void init_data();
-    void preprocess_fg(cv::Mat const &kernel);
-    void preprocess_input();
     void put_inout_msg();
-    void remove_outlier_blobs();
-    void track(std::string const &video,
-               size_t neglect_frame_size);
+    void track(std::string const &video);
     void track_people(cv::Mat const &kernel);
 
-    std::vector<cv::Rect> bound_rects_;
-    cv::Mat contour_img_;
-    std::vector<std::vector<cv::Point>> contours_;
-    bool debug_ = true;
-    cv::Mat fg_;
-    cv::Ptr<cv::BackgroundSubtractor> fgbg_;
+    std::unique_ptr<blob_detector_base> blob_detector_;
     cv::Mat gray_img_;
     cv::Mat img_;
     size_t in_blob_size_ = 0;
     double min_human_size_ = 4000;
-    int offset_ = 50;
-    size_t out_blob_size_ = 0;
-    size_t process_frame_ = 0;
+    int detect_margin_ = 50;
+    size_t out_blob_size_ = 0;    
     std::unique_ptr<tracker_base> tracker_;
 };
 
